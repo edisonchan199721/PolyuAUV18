8da71f2261a3417ea73400c8b7a9f890e49ae4c1
Edison <edisonchan199721@gmail.com>
2018-03-01 17:36:57 +0800

    Added detection

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import picamera
 import threading
 import time
+import detect
 
 class camera_thread(threading.Thread):
     def __init__(self):
@@ -13,14 +14,20 @@ class camera_thread(threading.Thread):
 
     def stop(self):
         self._is_running = False
-       
+
 def photoCapture():
-    with picamera.PiCamera() as camera:
-        camera.resolution = (640,480)
-        camera.framerate = 10
-        camera.shutter_speed = camera.exposure_speed
-        #camera.exposure_mode = 'off'
+    camera = PiCamera()
+    camera.resolution = (640,480)
+    camera.framerate = 10
+    camera.shutter_speed = camera.exposure_speed
+    #camera.exposure_mode = 'off'
+    rawCapture = PiRGBArray(camera)
+    time.sleep(2)
+    while (True):
+        camera.capture(rawCapture, format="bgr")
+        image = rawCapture.array
         time.sleep(2)
-        for filename in camera.capture_continuous('img{counter:03d}.jpg'):
-            print('Captured %s' % filename)
-            time.sleep(2)
+        print (detect.get_extreme_red_points(image))
+        # for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+        #     print('Captured %s' % filename)
+        #     time.sleep(2)
